b193f27931d1f8fdf12fedee894b0cbc5087ba23
Ming Li <ming.george.li@gmail.com>
2022-01-30 20:18:05 +0000

    update script and make exec

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PYTHONPATH=$SCRIPT_DIR python app/main.py
-
+PYTHONPATH=$SCRIPT_DIR python -m app.main
