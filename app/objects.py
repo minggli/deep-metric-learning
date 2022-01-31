a9c2ea3449cf4176dbc754caf2f3f3da025ac223
Ming Li <ming.george.li@gmail.com>
2022-01-31 02:12:06 +0000

    specify device for dataset and model

--- app/objects.py
+++ app/objects.py
@@ -1,4 +1,11 @@
+import torch
 from torchvision import transforms as T
+
+from app.utils import get_torch_device
+
+
+def assign_device(tensor: torch.Tensor, device=get_torch_device()):
+    return tensor.to(device)
 
 
 # pylint: disable=too-few-public-methods
@@ -9,11 +16,7 @@ class BaseTransform(T.Compose):
 class ImageTransform(BaseTransform):
     def __init__(self, transforms):
         _ = transforms
-        super().__init__(
-            [
-                T.ToTensor(),
-            ]
-        )
+        super().__init__([T.ToTensor(), T.Lambda(assign_device)])
 
 
 class TextTransform(BaseTransform):
--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,13 @@
 from pathlib import Path
+import torch
+
+torch_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def get_torch_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda:0")
+    return torch.device("cpu")
 
 
 def get_project_root() -> Path:
