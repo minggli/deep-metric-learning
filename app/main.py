16847c8c2188fbc70574f5f71ae3f842f10b7917
Ming Li <ming.george.li@gmail.com>
2022-02-05 18:38:57 +0000

    add training animations to gcs

--- app/main.py
+++ app/main.py
@@ -6,11 +6,16 @@ from smart_open import open
 from torch import nn
 from torch.utils.data import DataLoader
 
+import seaborn as sns
+
+
 from app.datasets import ExperimentDatasets, load_dataset
-from app.ml_ops import test, train
+from app.ml_ops import test, train, visualise_embedding
 from app.models import InfoNCELoss, Network, SoftNearestNeighborsLoss, resnet18
 from app.objects import ImageTransform, TargetTransform
 from app.utils import get_project_root, get_torch_device
+
+sns.set()
 
 PROJ_ROOT = get_project_root()
 with open(PROJ_ROOT / "app/model_cfg.yaml", encoding="utf-8") as f:
@@ -23,7 +28,7 @@ if __name__ == "__main__":
     )
     train_batch_iter, test_batch_iter = DataLoader(
         ds_train, batch_size=model_config["batch_size"], shuffle=True, num_workers=0
-    ), DataLoader(ds_test, batch_size=model_config["batch_size"], shuffle=True, num_workers=0)
+    ), DataLoader(ds_test, batch_size=model_config["batch_size"], shuffle=False, num_workers=0)
 
     model = Network(resnet18).to(get_torch_device())
     model = nn.DataParallel(model)
@@ -33,9 +38,19 @@ if __name__ == "__main__":
     loss = nn.DataParallel(loss)
 
     timestamp = int(time())
+    for x_test, y_test in test_batch_iter:
+        break
 
+    images = []
     for epoch in range(10):
         train(train_batch_iter, model, loss, optimizer)
+        visualise_embedding(epoch, images, x_test, y_test, model)
 
+    with open(f"gs://saved_models_minggli/images_{timestamp}.gif", "wb") as f:
+        images[0].save(f,
+                format='GIF',
+                append_images=images[1:],
+                save_all=True, duration=500, loop=0)
+    
     with open(f"gs://saved_models_minggli/model_{timestamp}.pt", "wb") as f:
         torch.save(model, f)
--- app/ml_ops.py
+++ app/ml_ops.py
@@ -1,4 +1,8 @@
 import torch
+from torch import nn
+from PIL import Image
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
 
 def train(dataloader, model, loss_fn, optimizer):
@@ -33,3 +37,28 @@ def test(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+def visualise_embedding(epoch: int, images: list, x_test: torch.Tensor, y_test: torch.Tensor, model: nn.Module):
+    with torch.no_grad():
+        model.eval()
+        embedding_x, output_2 = model(x_test)
+        array = embedding_x.cpu().numpy()
+        label = list(map(str, y_test.cpu().numpy().astype('int').tolist()))
+
+    tsne = TSNE(n_components=2, perplexity=50., init='pca', random_state=0)
+    reduced_array = tsne.fit_transform(array)
+
+    fig = plt.figure(epoch, figsize=(12, 8), dpi=300)
+    ax = fig.gca()
+    sns.scatterplot(x=reduced_array[:, 0], y=reduced_array[:, 1], hue=label, ax=ax)
+    ax.set_title(f"Epoch: {epoch + 1}")
+    fig.canvas.draw()
+
+    img = Image.frombytes('RGB', 
+        fig.canvas.get_width_height(),
+        fig.canvas.tostring_rgb())
+
+    images.append(img)
+
+
